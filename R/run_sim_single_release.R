096eea74b765a5ff0e2641f49b4c13611005097a
lucymerobinson <lucy.robinson@ccamlr.org>
2017-04-09 17:21:32 +1000

    sim for single_release estimates has been updated and is working for estimates in season 2 now

--- R/run_sim_single_release.R
+++ R/run_sim_single_release.R
@@ -17,20 +17,21 @@ run_sim_single_release <- function(control, n_reps, run_assessment){
       ## Simulation Order
       ### 1 New season processes
       ## 1.1 create a temp vector to store the population and tag numbers in year one this is the initial pop size in the control file
-      temp_untagged <- model$N[y,]
       if(y==1){
-        # zero until tags released, however, doesn't cause problems
-        ## calculate the adjusted releases based on tag-induced mortality
+        temp_untagged <- model$N_true[y,]
+        # zero temp_tags until tags released, however, doesn't cause problems
         temp_tags <- model$tags_available[y,]
-        # pop_size <- temp_untagged
-      }else{
-      temp_tags <- model$tags_available[y-1,] 
-        # pop_size <- temp_untagged + temp_tags
-        # 1.2 Estimate recruitment 
       }
+      
+      if(y<1){
+        temp_untagged <- model$N_end_season[y-1,]
+        temp_tags <- model$tags_available[y-1,] 
+      }
+      
+      # 1.2 Estimate recruitment
       rec <-est_recruits(type=control[["rec_pars"]]$type,
                          rec_pars=control[["rec_pars"]],
-                         var=control[["stochastic_rec"]])
+                         var=control[["rec_pars"]]$variation)
       ## assign it to areas (this can be replaced with a function)
       rec_area <- ceiling(rec * control$rec_area)
       
@@ -41,14 +42,12 @@ run_sim_single_release <- function(control, n_reps, run_assessment){
       temp_untagged <- temp_untagged + rec_area
       # apply all natural mortality 
       temp_untagged <- temp_untagged* exp(-control[["pop_pars"]]$nat_mort)
-      # temp_tags <- temp_tags * exp(-control[["pop_pars"]]$nat_mort/2) - removing as using tags avail as calculated in tagr which should include NM
-      # maybe try the calculation of true population from here
-      ## total population
+      
+      ## true total population to be compared with estimates  
       final_N <- temp_untagged
       
       ## 2 Harvest and natural mortality
       ## 2.1 calculate population size by strata (tagged + untagged)
-      # temp_pop <- temp_untagged + temp_tags
       ## 2.1.1 extract last seasons abundance estimate
       if(y>2){
         est_abund <- model$abund_est[y-1,]}else{
@@ -77,38 +76,25 @@ run_sim_single_release <- function(control, n_reps, run_assessment){
       
       recaps_by_area <- rep(0, control[["n_regions"]])
       
-      if(y>=2){
-        rprob_by_area <- temp_tags / temp_untagged
-        
-        for(k in 1:control$n_regions){
-          recaps_by_area[k] <- rbinom(n=1, size=round(catch_by_area[k]),
-                                      prob=rprob_by_area[k])
-          recaps_by_area[is.na(recaps_by_area[k])]<-0
-        }
+      
+      rprob_by_area <- temp_tags / temp_untagged
+      
+      # if(is.na(rprob_by_area)){
+      # rprob_by_area <- 0
+      
+      for(k in 1:control$n_regions){
+        recaps_by_area <- rbinom(n=1, size=round(catch_by_area[k]),
+                                 prob=rprob_by_area[k])
       }
-      #if(any(prob_recap < 0 | prob_recap > 1))
-      #stop("Probability of tag recapture in cell must be between zero and one")
-      ## 2.2 calculate & remove total catch & tags & apply natural mortality
-      # if(control[["harvest_pars"]]$ricker ==1){
-      ## ricker type 1 all fishing first then
-      ## calculate tag recaptures / attrition
-      ## object for recaptures
-      
-      
-      # ## then remove them restricting to max number of tags 
-      # recaps_by_area <- pmin(recaps_by_area, temp_tags)
+      # }
+      recaps_by_area <- pmin(recaps_by_area, temp_tags)
       ## add these back to the untagged population
       # remove catch 
-      temp_untagged <- temp_untagged - catch_by_area
+      temp_untagged <- temp_untagged - catch_by_area + recaps_by_area
       # add releases back in accounting for tagging mortaility 
-      temp_N <- temp_untagged + tag_releases*exp(-control[["tag_pars"]]$mort)
-      # subtract recaptures from the population 
-      if(y>=2){
-        temp_N <- temp_N-recaps_by_area}
-      # temp_tags <- temp_tags - recaps_by_area - recaptures subtraction is being handled in tagr
-      # apply natural mortality to untagged population
-      #cat("before final_N ", temp_untagged, "\n")
-       # final_N <- temp_N
+      temp_tagged <- tag_releases - recaps_by_area
+      
+      total_pop <- temp_untagged + temp_tagged
       
       # get tag releases ready for the tags_available in the following season 
       if(y==1){
@@ -119,7 +105,8 @@ run_sim_single_release <- function(control, n_reps, run_assessment){
                                    -control[["tag_pars"]]$shed)}
       
       # ## 3 Assign the end of season population size to the storage
-      model$N[y,] <- final_N
+      model$N_end_season[y,] <- total_pop
+      model$N_true[y,] <- final_N
       model$releases[y,] <- tag_releases
       # model$ recaps is a 4 dim array with year of release as rows year of recapture as columns and the release region and recapture region in the 3rd and fourth dim of the array
       if(y==1){model$tags_available[y,] <- temp_tags}
