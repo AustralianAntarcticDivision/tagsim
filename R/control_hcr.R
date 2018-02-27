be7664fbd12fb3a91fab348018e9a705764f8d3d
Burch, Paul (O&A, Hobart) <Paul.Burch@csiro.au>
2018-02-27 15:16:03 +1100

    simplified versions of functions to  create the model and storage objects

---
+++ R/control_hcr.R
@@ -0,0 +1,77 @@
+#' Create a simplified control file for a single-area population without tags
+#'
+#' The control file specifies the simulation parameters
+#'
+#' A simulation is run using the function (see \code{\link{run_sim}})
+#' @param years vector of years (integers starting with 1)
+#' @param pop_pars list of population parameters containing the initial population
+#' size ("initial") and instantaneous natural mortality ("nat_mort")
+#' @param rec_pars list of recruitment parameters containing the name of the mechanism
+#' for calculating recruitment ("type"; can be either "constant", "logistic", "bevholt"
+#' or "ricker") see \code{\link{est_recruits}}, the spatial distribution of the recruitment
+#' is assigment with "spat_dist" which can be "uniform", "lognormal" or "user" specified
+#' (see \code{\link{recruit_area}}). Recruitment The stochasisity in recruitment is determined
+#' @param harvest_pars harvest parameters a list containing "type" ("const_exploit" =
+#' constant exploitation rate, "TAC" = constant catch, ...),  (see \code{\link{calc_catch}})
+#' @param fish_pars list of parameters relating to the fishing strategy . The intent is for
+#' these to control the spatial dynamics of fishing and for harvest_pars to determine total
+#' catch (see \code{\link{fishing_locs}})
+#' @param assess_pars list of assessment parameters (see \code{\link{do_assessment}})
+#' @param stoch_rec stochasitc recruitment (default=TRUE)
+#' @aliases control
+#' @export
+create_control_hcr <- function(years,
+                               pop_pars,
+                               rec_pars=list("type"="constant", "mu"=0, "s"=1, "spat_dist"="uniform"),
+                               harvest_pars,
+                               fish_pars,
+                               assess_pars,
+                               stoch_rec = TRUE){
+  ## add additional error checking
+  ## create list object
+  control <- list("years" = years,
+                  "pop_pars" = pop_pars,
+                  "harvest_pars" =harvest_pars,
+                  "fish_pars" = fish_pars,
+                  "assess_pars" = assess_pars,
+                  "n_years" = length(years))
+  ## add a class
+  class(control) <- "control"
+  ## return the control file
+  control
+}
+
+#' Create a simplified model object for a single-area population without tags
+#'
+#' Create a list of arrays of the appropriate dimensions for storing population
+#' and fishery information
+#'
+#' Note tags could be a multi-dimensional array to account for tag movement
+#' @param control a control file
+#' @export
+create_model_hcr <- function(control){
+  ## create storage for the
+  init_N <- matrix(0, control[["n_years"]] + 1, control[["n_regions"]])
+  init_R <- matrix(0, control[["n_years"]] + 1, control[["n_regions"]])
+  init_A <- matrix(0, control[["n_years"]] +1 , control[["n_regions"]])
+  ## fill year zero
+  if(control[["rec_pars"]]$stochastic_rec){
+    init_N[1,] <- control[["pop_pars"]]$initial *
+                  rlnorm(1, control[["rec_pars"]]$mu, control[["rec_pars"]]$s)
+  }else{
+    init_N[1,] <- control[["pop_pars"]]$initial
+  }
+  ## add initial recruitment (not getting used currently)
+  init_R[1,] <- est_recruits(type=control[["rec_pars"]]$type,
+                            N_area = init_N[1,], rec_pars=control[["rec_pars"]])
+  ## initial assessment knows pop size without error ** can change this
+  init_A[1,] <- init_N[1,]
+  ## create the object
+  obj <- list("N" = init_N,
+              "recruits" = init_R,
+              "catch" = matrix(0, control[["n_years"]] + 1, control[["n_regions"]]),
+              "abund_est" = init_A
+  )
+  ## return the object
+  return(obj)
+}
