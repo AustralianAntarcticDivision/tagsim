6c2e90d9209645b39c583e6887dca672a2489ada
Burch, Paul (O&A, Hobart) <Paul.Burch@csiro.au>
2018-02-28 13:33:58 +1100

    functions to convert between catch and F

---
+++ R/functions.R
@@ -0,0 +1,45 @@
+#' Baranov catch equation
+#'
+#' Returns catch for given F for population size N
+#' with natural and fishing mortality M and F respectively
+#' @param N population size
+#' @param nat_mort instantaneous natural mortality
+#' @param fish_mort instantaneousfishing mortality
+#' @export
+baranov_catch <- function(N, nat_mort, fish_mort)
+{
+  N*fish_mort/(nat_mort+fish_mort)*(1-exp(-nat_mort-fish_mort))
+}
+
+#' Calculates catch for a given F
+#'
+#' Returns catch from N for a given F, used in \code{catchtoFfinding} the F that
+#' corresponds to a given catch.
+#'
+#' Modified from function written by Bill de la Mare
+#' @param N population size
+#' @param nat_mort instantaneous natural mortality
+#' @param fish_mort instantaneous fishing mortality
+#' @export
+F_to_catch <- function(N, nat_mort, fish_mort){
+  ## calculate total mortality Z
+  Z <- fish_mort + nat_mort
+  # calculate the fraction of the population that dies from fishing
+  N * (1-exp(-Z))*fish_mort/Z
+}
+
+#' Calculate F for a given catch
+#'
+#' Root of this function is the F that corresponds to a given catch
+#'
+#' Modified from function written by Bill de la Mare
+#' @param N population size
+#' @param nat_mort instantaneous natural mortality
+#' @param fish_mort instantaneous fishing mortality
+#' @param catch catch
+#' @export
+catch_to_F <- function(N, nat_mort, fish_mort, catch)
+{
+  catch - F_to_catch(N, nat_mort, fish_mort)
+}
+
