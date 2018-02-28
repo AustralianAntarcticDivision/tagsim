553f8a1b30cdb1f23b89163487ba0cead70403fa
Burch, Paul (O&A, Hobart) <Paul.Burch@csiro.au>
2018-03-01 09:55:23 +1100

    renaming "resilience" to "r" and carrying capacity "K" to "k" for consistency with the literature

--- R/recruitment.R
+++ R/recruitment.R
@@ -1,8 +1,8 @@
 #' Calculate recrutiment from previous years population
 #'
 #' Function returns number of recruits given last year's population size N,
-#' carrying capacity K, natural survivorship S and resilience r.
-#' rk is the number of recruits per unit spawner when B = K.
+#' carrying capacity k and resilience r. rk is the number of recruits per unit
+#' spawner when B = K.
 #'
 #' Modification of function written by Bill de la Mare
 #' @param type recruitment function either "constant", "logistic", "bevholt"
@@ -18,22 +18,22 @@ est_recruits <- function(type, N_area, rec_pars){
   switch(type,
          constant = recruits <- rec_pars[["initial"]],
          logistic = {
-           recruits <- N*rec_pars[["rk"]]*(1 + rec_pars[["resilience"]]*(1 - N/rec_pars[["K"]]))
+           recruits <- N*rec_pars[["rk"]]*(1 + rec_pars[["r"]]*(1 - N/rec_pars[["k"]]))
          },
          bevholt = {
-           ## If resilience, K and rk are supplied we use them to calculate a and b
-           if(all(c("resilience", "K", "rk") %in% names(rec_pars))){
-           bta <- rec_pars[["resilience"]]*rec_pars[["rk"]]
-           b <- 1./(bta/(rec_pars[["rk"]]*rec_pars[["K"]]) - 1./rec_pars[["K"]])
+           ## If r, k and rk are supplied we use them to calculate a and b
+           if(all(c("r", "k", "rk") %in% names(rec_pars))){
+           bta <- rec_pars[["r"]]*rec_pars[["rk"]]
+           b <- 1./(bta/(rec_pars[["rk"]]*rec_pars[["k"]]) - 1./rec_pars[["k"]])
            a <- b*bta
            }
            recruits<-(rec_pars[["a"]]*N)/(rec_pars[["b"]] + N)
          },
          ricker = {
-           ## If resilience, K and rk are supplied we use them to calculate a and b
-           if(all(c("resilience", "K", "rk") %in% names(rec_pars))){
-           a <- rec_pars[["resilience"]]*rec_pars[["rk"]]
-           b <- log(a*rec_pars[["K"]]/(rec_pars[["rk"]]*rec_pars[["K"]]))/rec_pars[["K"]]
+           ## If r, k and rk are supplied we use them to calculate a and b
+           if(all(c("r", "k", "rk") %in% names(rec_pars))){
+           a <- rec_pars[["r"]]*rec_pars[["rk"]]
+           b <- log(a*rec_pars[["k"]]/(rec_pars[["rk"]]*rec_pars[["k"]]))/rec_pars[["k"]]
            }
            recruits<-rec_pars[["a"]]*N*exp(-rec_pars[["b"]]*N)
          }
