919b1a031f65ce17960bfcb6a95003c715da72bb
Burch, Paul (O&A, Hobart) <Paul.Burch@csiro.au>
2018-03-02 13:05:42 +1100

    many bug fixes

--- R/run_hcr.R
+++ R/run_hcr.R
@@ -14,7 +14,7 @@
 #' ## add an example
 run_hcr <- function(control, n_reps, save=FALSE, path=NULL, filename=NULL){
   ##* storage for the final results
-  storage <- create_storage(control, n_reps)
+  storage <- create_storage_hcr(control, n_reps)
   ## loop over the simulations
   for(i in 1:n_reps){
     #print(paste("## Simulation ",iSim," ##",sep=""))
@@ -26,31 +26,31 @@ run_hcr <- function(control, n_reps, save=FALSE, path=NULL, filename=NULL){
       ## Simulation Order
       ### 1 New season processes
       ## 1.1 create a temp vector to store the populationnumbers
-      temp_N <- model$N[y-1]
+      N_yr <- model$N[y-1]
       ## 1.2 Estimate recruitment from last season numbers (there is no growth)
-      rec <- est_recruits(type=control[["rec_pars"]]$type, N_area = temp_N,
-                          rec_pars=control[["rec_pars"]])
+      rec_yr <- est_recruits(type=control[["rec_pars"]]$type, N_area = N_yr,
+                             rec_pars=control[["rec_pars"]])
       ## there is no movement and only a single area so add the recruits
       ## 1.3 add the recrutiment
       ##* consider ssaving the recruitments
-      temp_N <- temp_N + rec
+      N_yr <- N_yr + rec_yr
       ## 2 Harvest and natural mortality
-      ##* check that the maximum catch is implemented here
-      catch_limit <- calc_catch(control, temp_N, model$abund_est[y-1])
       ## 2.2 calculate & remove total catch & apply natural mortality
       ## apply 1/2 M, the F, then 1/2 M
-      temp_N <- temp_N * exp(-0.5*control[["pop_pars"]]$nat_mort)
-      ## remove catch
-      temp_N <- temp_N - catch_limit
+      N_yr <- N_yr * exp(-0.5*control[["pop_pars"]]$nat_mort)
+      ## calculate then remove the catch
+      catch_yr <- calc_catch(control, N_yr, model$abund_est[y-1])
+      N_yr <- N_yr - catch_yr
       ## apply 1/2 natural mortality
-      temp_N <- temp_N * exp(-0.5*control[["pop_pars"]]$nat_mort)
+      final_N <- N_yr * exp(-0.5*control[["pop_pars"]]$nat_mort)
       ## 3 Assign the end of season population size to the storage
       model$N[y] <- final_N
-      model$catch[y] <- catch_by_area
-      model$recruits[y] <- rec_area
+      model$catch[y] <- catch_yr
+      model$recruits[y] <- rec_yr
+      ## note the assessment happens at the end of the year (after both F&M)
       model$abund_est[y] <- do_assessment(control, model, year=y)
     }
-    storage <- store_rep(storage, control, model, rep=i)
+    storage <- store_rep_hcr(storage, control, model, rep=i)
   }
   ## return the storage
   storage
